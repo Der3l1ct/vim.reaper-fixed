e0a8c269e5ec061519096bc29fd720af0bfc1aa6
CodesAndTokes <127527969+Der3l1ct@users.noreply.github.com>
2024-11-13 02:27:26 -0700

    Update update_config.sh
    added vim-plug install

--- update_config.sh
+++ update_config.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo pacman -S fzf ccls jdk-openjdk lazygit
 yay -S lazydocker
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       
 git pull 
 cp -r nvim $HOME/.config/
 echo "nvim folder copied to $HOME/.config/"
