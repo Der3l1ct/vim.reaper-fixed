635e5c8b6c1c55a0bb788dfc4023671aa842164e
CodesAndTokes <127527969+Der3l1ct@users.noreply.github.com>
2024-11-13 19:35:27 -0700

    Update update_config.sh

--- update_config.sh
+++ update_config.sh
@@ -5,7 +5,7 @@ yay -S lazydocker
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-npm init @eslint/config@latest
+npm install -g eslint
 npm install --save-dev coc.nvim
        
 git pull 
